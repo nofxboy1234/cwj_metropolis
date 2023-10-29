919cc7144fab46cdb726c08e3c3a7004fd759ac5
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-29 19:28:21 +0200

    Add failing test

--- spec/features/create_city_spec.rb
+++ spec/features/create_city_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe 'Creating a city', type: :feature do
     visit cities_path
     expect(page).to have_content('Minneapolis')
   end
+
+  scenario 'invalid inputs' do
+    visit new_city_path
+    fill_in 'Name', with: ''
+    click_on 'Create City'
+    expect(page).to have_content("Name can't be blank")
+  end
 end
