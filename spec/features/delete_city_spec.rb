455b40cbbda76ce6172021b4949aceccb3eda048
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-30 09:16:30 +0200

    Add confirm modal to delete city button
    Add sleep call in test to see it happen.
    Add accept_alert to test.
    Set Capybara.default_driver = :selenium_chrome

--- spec/features/delete_city_spec.rb
+++ spec/features/delete_city_spec.rb
@@ -4,13 +4,12 @@ RSpec.describe 'Deleting a city', type: :feature do
   scenario 'success' do
     City.create!(name: 'NYC')
     visit cities_path
+    sleep(3)
     expect(page).to have_content('NYC')
 
     click_on 'Destroy'
-
-    # accept_confirm do
-    #   click_on 'Destroy'
-    # end
+    sleep(3)
+    accept_alert
     expect(page).not_to have_content('NYC')
   end
 end
